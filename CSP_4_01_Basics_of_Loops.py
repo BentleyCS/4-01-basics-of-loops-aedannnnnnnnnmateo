8a83bfb5a2e3e946ee3e6189f6f4182e66c4c23f
aedannnnnnnnnmateo <amateo26@bentleyschool.org>
2025-12-17 12:36:51 -0800

    Add files via upload

--- CSP_4_01_Basics_of_Loops.py
+++ CSP_4_01_Basics_of_Loops.py
@@ -7,7 +7,12 @@ def oddNumbers(n:int) ->str:
     example oddNumbers(8) -> "1 3 5 7"
     example oddNumbers(-8) -> ""
     """
-
+    answer = ""
+    for i in range (1,n+1,2):
+        answer += str(i) + " "
+    answer = answer[0:len(answer)-1]
+    return answer
+print(oddNumbers(9))
 
 def backwards(n)-> int:
     """
@@ -17,8 +22,13 @@ def backwards(n)-> int:
     example backwards(8) -> "8 7 6 5 4 3 2 1"
     example backwards(-2) -> ""
     """
-
-
+    answer = ""
+    while n>=1:
+        answer+= str(n) + " "
+        n-= 1
+    answer = answer [0: len(answer) - 1]
+    return answer
+print(backwards(5))
 
 def randomRepeating():
     """
@@ -28,7 +38,11 @@ def randomRepeating():
     :return:
     """
     tries = 0
-    print(f"it took {tries} tries to get a 10")
+    while True:
+        number = random.randint(1,10)
+        tries += 1
+        if number ==10:
+            print(f"it took {tries} tries to get a 10")
 def randomRange(n):
     """
     Generate a random number from 1 to 100 n number of times. Then after that is
@@ -37,13 +51,27 @@ def randomRange(n):
     :param n:
     :return:
     """
+    highest = -1
+    lowest = 101
+    for i in range(n):
+        num = random.randint(1, 100)
+        print(num)
+        if num > highest:
+            highest = num
+        if num < lowest:
+            lowest = num
+    print(f"Highest: {highest}")
+    print(f"Lowest: {lowest}")
 def reverse(word:str)->str:
     """
     Takes in a string as an argument and return the given string in reverse.
     example reverse("cat") -> "tac"
     example reverse("Hello") -> "olleH"
     """
-
+    result = ""
+    for i in range(len(word) - 1, -1, -1):
+        result += word[i]
+    return result
 def fizzBuzzContinuous(n):
     """
     Modify the function such that it does the fizzbuzz operation on all numbers
@@ -58,6 +86,17 @@ def fizzBuzzContinuous(n):
     :param n:
     :return:
     """
+    result = []
+    for i in range(1, n + 1):
+         if i % 3 == 0 and i % 5 == 0:
+            result.append("fizzbuzz")
+         elif i % 3 == 0:
+            result.append("fizz")
+         elif i % 5 == 0:
+            result.append("buzz")
+         else:
+            result.append(str(i))
+    return " ".join(result)
 
 def collatz(n):
     """
@@ -66,11 +105,19 @@ def collatz(n):
     The collatz conjecture is that if n is an even number divide it by 2. if n is
     an odd number multiply it by 3 and add 1.
     Repeat this process until n == 1.
-    :param n:
+    :paramn:
     :return:
     """
-
-
+    result = []
+    while n >= 1:
+        result.append(str(n))
+        if n == 1:
+                break
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+    return " ".join(result)
 def fibonacci(n):
     """
     for the given argument n print out the first n numbers of the fibonacci
@@ -83,6 +130,14 @@ def fibonacci(n):
     :param n:
     :return:
     """
-
-
+    if n <= 0:
+        return ""
+    if n == 1:
+        return "0"
+    result = []
+    a, b = 0, 1
+    for i in range(n):
+        result.append(str(a))
+        a, b = b, a + b
+    return " ".join(result)
 print(fibonacci(300))
